37f4aa41e62d62b3dfd5faaa176cbd4cd6257c72
chikechris <cchike2017@gmail.com>
2019-09-02 00:55:11 -0400

    the POST requests creates user at "/api/users

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ const db = require('./data/db.js')
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('its working')
 })
+
+app.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (user.name && user.bio) {
+    db.insert(user)
+      .then(user => res.status(201).json(user))
+      .catch(err => {
+        return res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        });
+      });
+  } else {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+});
 
 app.listen(5500, () => {
   console.log(`The app is up and running on port 5500`)
