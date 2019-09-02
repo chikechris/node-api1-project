e845d28ca8028b067b165c7d07053f19cb02c159
chikechris <cchike2017@gmail.com>
2019-09-02 00:10:51 -0400

    imported express, created express server, port connected on port 5500

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // implement your API here
 
+const db = require('./data/db.js')
+const express = require('express')
+const app = express()
 
+app.get('/', (req, res) => {
+  res.send('its working')
+})
+
+app.listen(5500, () => {
+  console.log(`The app is up and running on port 5500`)
+})
