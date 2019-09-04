95a83fd1cba38fbc8040817b646e98b94bcff884
chikechris <cchike2017@gmail.com>
2019-09-04 11:51:33 -0400

    done with project

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get('/', (req, res) => {
   res.send('its working');
 });
 
+//Post Request
 app.post('/api/users', (req, res) => {
   const user = req.body;
   if (user.name && user.bio) {
@@ -27,6 +28,7 @@ app.post('/api/users', (req, res) => {
   }
 });
 
+//Get request
 app.get('/api/users', (req, res) => {
   db.find()
     .then(users => res.status(200).json(users))
@@ -37,6 +39,7 @@ app.get('/api/users', (req, res) => {
     });
 });
 
+//Get request by Id
 app.get('/api/users/:id', (req, res) => {
   const id = req.params.id;
   db.findById(id)
@@ -56,7 +59,55 @@ app.get('/api/users/:id', (req, res) => {
     });
 });
 
+//Delete request by Id
+app.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  db.remove(id)
+    .then(deleted => {
+      console.log(deleted);
+      if (deleted === 0) {
+        return res
+          .status(404)
+          .json({ message: 'The user with the specified id dose not exit' });
+      }
+      res.status(204).end();
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: 'The user information could not be retrieved.' });
+    });
+});
 
+//Put request by Id
+app.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const edits = req.body;
+
+  if (!edits.name || !edits.bio) {
+    return res
+      .status(400)
+      .json({ message: 'The user with the specified ID does not exist.' });
+  }
+
+  db.update(id, edits)
+    .then(updated => {
+      if (!updated) {
+        return res
+          .status(404)
+          .json({ message: 'The user with the specified id dose not exit' });
+      } else {
+        res.status(200).json(updated);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: 'The user information could not be retrieved.' });
+    });
+});
 
 app.listen(5500, () => {
   console.log(`The app is up and running on port 5500`);
